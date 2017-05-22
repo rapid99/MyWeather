dcd1ffe3d1caab21299451446ab97db8e4dc912c
“Matt <matthyman03@gmail.com>
2017-05-21 22:38:08 -0600

    initial commit

---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        public string City { get; set; }
+        public decimal Temp { get; set; }   
+        public decimal Humid { get; set; }
+    }
+}
